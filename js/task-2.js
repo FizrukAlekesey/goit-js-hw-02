ee415ba3a5828d39725da2f2562da38b21702f3c
Fizruk <fizrukprogram@gmail.com>
2024-05-02 08:40:09 +0200

    2

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,9 @@
 function formatMessage(message, maxLength){
-  
-if (message <= maxLength) {
+    //const
+if (message.length <= maxLength) {
     return message;
 } else {
-    return message + "..."
+    return message.slice(0, maxLength) + "..."
 }
 
 }
